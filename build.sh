8b73f7b19a04fe166f1f44a4ad6b1054977b02cd
Harshit00yadav <hash00usr@gmail.com>
2025-06-30 16:37:15 +0530

    build file update

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
-gcc -o build/app src/main.c -lSDL2
-echo "Build complete"
+if [ ! -d "./build" ]; then
+	echo "making build directory"
+	mkdir build
+fi
+if [ -d "./build" ]; then
+	echo "./build directory exists."
+	gcc -o build/app src/main.c -lSDL2
+	echo "Build complete"
+fi
 if [[ "$1" == "run" ]]; then
 	./build/app
 else
